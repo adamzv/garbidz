97fcf015e5e8abcc71e5d20070f6ffa89b3a6a29
Ľudovít Laca <laca.ludo97@gmail.com>
2021-04-05 23:04:15 +0200

    Added User, Report model with repository and controller
    -  bug fix in AddressController where  setTown did not used new address

---
+++ backend/src/main/java/com/github/adamzv/backend/model/User.java
@@ -0,0 +1,89 @@
+package com.github.adamzv.backend.model;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+
+@Entity(name = "user_account")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String surname;
+
+    @NotNull
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @ManyToOne
+    @JoinColumn(name = "id_role", nullable = false)
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "id_address", nullable = false)
+    private Address address;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+}
